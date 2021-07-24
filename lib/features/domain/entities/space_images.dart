af0adaee119c2e6d6cb866d622097574a66c7a38
Daniel Marques <daniel.silva.city@gmail.com>
2021-07-23 23:36:47 -0300

    Create Entity Space Images

---
+++ lib/features/domain/entities/space_images.dart
@@ -0,0 +1,23 @@
+import 'package:equatable/equatable.dart';
+
+class SpaceImages extends Equatable {
+  final String description;
+  final String mediaType;
+  final String title;
+  final String mediaUrl;
+
+  SpaceImages({
+    required this.description,
+    required this.mediaType,
+    required this.title,
+    required this.mediaUrl,
+  });
+
+  @override
+  List<Object?> get props => [
+        description,
+        mediaType,
+        title,
+        mediaUrl,
+      ];
+}
