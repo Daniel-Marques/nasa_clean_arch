5189994eea8158bba85db4bc00adbbf8afd78bc2
Daniel Marques <daniel.silva.city@gmail.com>
2021-07-23 23:54:21 -0300

    Create class NoParams abstraction usecase

--- lib/core/usecase/usecase.dart
+++ lib/core/usecase/usecase.dart
@@ -1,6 +1,12 @@
 import 'package:dartz/dartz.dart';
+import 'package:equatable/equatable.dart';
 
 abstract class Usecase<Output, Input> {
   // Either: Return Left or Right
   Future<Either<Failure, Output>> call(Input params);
+}
+
+class NoParams extends Equatable {
+  @override
+  List<Object?> get props => [];
 }
