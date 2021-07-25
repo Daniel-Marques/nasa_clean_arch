627a14c04e2e3a7d544f1024b2b9f7c396d0d837
Daniel Marques <daniel.silva.city@gmail.com>
2021-07-25 18:13:04 -0300

    Created AppModule, AppWidget

--- lib/features/presenter/pages/home_page.dart
+++ lib/features/presenter/pages/home_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_modular/flutter_modular.dart';
+import 'package:nasa_clean_arch/features/presenter/controllers/home_store.dart';
 import 'package:nasa_clean_arch/features/presenter/widgets/custom_app_bar.dart';
 import 'package:nasa_clean_arch/features/presenter/widgets/round_button.dart';
 
@@ -8,7 +10,7 @@ class HomePage extends StatefulWidget {
   _HomePageState createState() => _HomePageState();
 }
 
-class _HomePageState extends State<HomePage> {
+class _HomePageState extends ModularState<HomePage, HomeStore> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -38,6 +40,9 @@ class _HomePageState extends State<HomePage> {
                       lastDate: DateTime.now(),
                     );
                   },
+                ),
+                SizedBox(
+                  height: 20,
                 ),
               ],
             ),
