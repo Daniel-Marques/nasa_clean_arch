4928bb3d6db490bd827aaad6b6e791b13020fab4
Daniel Marques <daniel.silva.city@gmail.com>
2021-07-24 22:56:35 -0300

    Created helpers apiKey

--- lib/features/data/datasources/space_media_datasource_implementation.dart
+++ lib/features/data/datasources/space_media_datasource_implementation.dart
@@ -1,9 +1,15 @@
+import 'package:nasa_clean_arch/core/http_client/http_client.dart';
+import 'package:nasa_clean_arch/core/utils/keys/nase_api_keys.dart';
+import 'package:nasa_clean_arch/features/data/datasources/endpoints/nase_endpoints.dart';
 import 'package:nasa_clean_arch/features/data/datasources/space_media_datasource.dart';
 import 'package:nasa_clean_arch/features/data/models/space_media_model.dart';
 
-class SpaceMediaDatasourceImplementation implements ISpaceMediaDatasource {
+class NasaDatasourceImplementation implements ISpaceMediaDatasource {
+  final HttpClient client;
+
+  NasaDatasourceImplementation(this.client);
   @override
-  Future<SpaceMediaModel> getSpaceMediaFromDate(DateTime date) {
-    throw UnimplementedError();
+  Future<SpaceMediaModel> getSpaceMediaFromDate(DateTime date) async {
+    return await client.get(NasaEndoints.apod(NasaApiKeys.apiKey, date));
   }
 }
