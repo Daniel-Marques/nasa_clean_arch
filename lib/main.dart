3d67ce190837edbc78229d5a4ce3aac3526a0f77
Daniel Marques <daniel.silva.city@gmail.com>
2021-07-25 17:58:10 -0300

    Created Home page

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:nasa_clean_arch/features/presenter/pages/home_page.dart';
 
 void main() {
   runApp(MyApp());
@@ -8,10 +9,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
+      home: HomePage(),
     );
   }
 }
